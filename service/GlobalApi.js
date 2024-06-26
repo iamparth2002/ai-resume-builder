984c74e6e738d7a2406ef9fcb7ed8ecc2ac7a326
iamparth2002 <neenaparth@gmail.com>
2024-06-26 17:32:21 +0530

    env updated

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const API_KEY=import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient=axios.create({
-    // baseURL:import.meta.env.VITE_API_BASE_URL+"/api/",
-    baseURL:"http://localhost:1337/api/",
+    baseURL:import.meta.env.VITE_BASE_URL+"/api/",
+    // baseURL:"http://localhost:1337/api/",
     headers:{
         'Content-Type':'application/json',
         'Authorization':`Bearer ${API_KEY}`

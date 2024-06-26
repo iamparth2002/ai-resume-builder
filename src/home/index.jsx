b842f919750b0fe593cf44955f1050b36ab7251f
iamparth2002 <neenaparth@gmail.com>
2024-06-26 17:53:42 +0530

    navigate

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -3,15 +3,14 @@ import { UserButton, useAuth } from '@clerk/clerk-react';
 import { AtomIcon, Edit, Share2 } from 'lucide-react';
 import React, { useEffect } from 'react';
 import { useUser } from '@clerk/clerk-react';
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from 'react-router-dom';
 import { redirect } from 'react-router-dom';
 
 function Home() {
-
   return (
     <div>
       <Header />
-      <div className='flex items-center justify-center w-full h-[600px]'>
+      <div className="flex items-center justify-center w-full h-[600px]">
         {/* <img src={'/grid.svg'} className="absolute z-[-10] w-full" 
       width={1200} height={300} /> */}
         {/* <Header/> */}
@@ -28,24 +27,27 @@ function Home() {
               Effortlessly Craft a Standout Resume with Our AI-Powered Builder
             </p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-              <a
-                href="/dashboard"
-                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-              >
-                Get Started
-                <svg
-                  className="ml-2 -mr-1 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <Link to={'/dashboard'}>
+                <a
+                  href="/dashboard"
+                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
+                  Get Started
+                  <svg
+                    className="ml-2 -mr-1 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </Link>
+
               <a
                 href="/"
                 className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
